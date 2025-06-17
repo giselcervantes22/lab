cc9ab27f2c470bfba193b040ff8c6b8e8b8530ad
giselcervantes22 <128953075+giselcervantes22@users.noreply.github.com>
2025-06-17 15:10:18 -0600

    Update calculate_Area.js

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,19 @@
-let length;
-let width;
+let count = 0; // Initialize count to 0
 
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
-   
-   let area = length * width;
-    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
-   }
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+  }
+
+function displayCount() {
+    document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
+    }
+
+function checkCountValue() {
+        if (count === 10) {
+          alert("Your Instagram post gained 10 followers! Congratulations!");
+        } else if (count === 20) {
+          alert("Your Instagram post gained 20 followers! Keep it up!");
+        }
+      }
